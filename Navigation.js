0aaa00d049d452ae83e4e108bb3367b903dbf24b
portoftheinvisiblemirror <150886443+portoftheinvisiblemirror@users.noreply.github.com>
2024-07-01 22:16:32 -0400

    Update Navigation.js

--- Navigation.js
+++ Navigation.js
@@ -3,7 +3,7 @@ document.write('<div class="navbar-fixed">' +
     '<a href="#" class="brand-logo"><img class="responsive-img" src = "img/southscienceclubnavbar.png" width = "330" height = "100"></img></a><a href="#" class="brand-logo right" style="font-size:12px"></a>' +
     '<a href="#" data-activates="nav-mobile" class="button-collapse "><i class="material-icons icon-dark">menu</i></a>' +
      '<ul class="right hide-on-med-and-down">' +
-   '<li class = "navlinks"><a href="index.html">Home</a></li>' 
+   '<li class = "navlinks"><a href="index.html">Home</a></li>' +
        '<li class = "navlinks"><a href="about-us.html" class >About Us</a></li>' +
        '<li class = "navlinks"><a href="science-bowl.html">Science Bowl</a></li>' +
    '<li class = "navlinks active"><a href="science-olympiad.html">Science Olympiad</a></li>' +
